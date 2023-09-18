9bfd762d0afdb281eb55d2309bcde7b8e97f8e0f
brunoVA101 <brunovalcantara@hotmail.com>
2023-09-18 11:45:07 -0300

    add more cases to tb

--- Exp03/interface_hcsr04_tb.vhd
+++ Exp03/interface_hcsr04_tb.vhd
@@ -63,7 +63,9 @@ architecture tb of interface_hcsr04_tb is
         (1, 5882),  -- 5882us (100cm)
         (2, 5899),  -- 5899us (100,29cm) truncar para 100cm
         (3, 4353),  -- 4353us (74cm)
-        (4, 4399)   -- 4399us (74,79cm)  arredondar para 75cm
+        (4, 4399),  -- 4399us (74,79cm)  arredondar para 75cm
+        (5, 588),   --  588us (10cm)
+        (6, 162)    --  162us (2,75cm)  arredondar para 3cm
         -- inserir aqui outros casos de teste (inserir "," na linha anterior)
       );
 
